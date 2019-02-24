efa723feb779f316206018febbf9114a762e6819
emajidev <emajidev@gmail.com>
2019-02-24 09:16:59 -0500

    push block

---
+++ blocki.js
@@ -0,0 +1,26 @@
+document.oncontextmenu = function(){
+    return false;
+    }
+
+document.onkeypress = function (event) {
+event = (event || window.event);
+if (event.keyCode == 123) {
+   //alert('No F-12');
+    return false;
+}
+
+}
+document.onmousedown = function (event) {
+event = (event || window.event);
+if (event.keyCode == 123) {
+    //alert('No F-keys');
+    return false;
+}
+}
+document.onkeydown = function (event) {
+event = (event || window.event);
+if (event.keyCode == 123) {
+    //alert('No F-keys');
+    return false;
+}
+}
